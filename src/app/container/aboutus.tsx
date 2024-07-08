63b917947d1ed1f5a122414cf4c8720ebdfd8bb5
Shahid shah <114140444+Shahid7424@users.noreply.github.com>
2024-07-08 13:29:54 +0530

    add section

--- src/app/container/aboutus.tsx
+++ src/app/container/aboutus.tsx
@@ -5,7 +5,6 @@ import { AboutImg } from "../Utilis/img";
 import Link from "next/link";
 
 const AboutUs = () => {
-
   return (
     <div className="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
       <div className="container py-5">
@@ -13,25 +12,43 @@ const AboutUs = () => {
           <div className="col-lg-7">
             <div className="section-title position-relative pb-3 mb-5">
               <h5 className="fw-bold text-primary text-uppercase">About Us</h5>
-              <h1 className="mb-0">The Best IT Solution With 10 Years of Experience</h1>
+              <h1 className="mb-0">
+                The Best IT Solution With 10 Years of Experience
+              </h1>
             </div>
-            <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos labore.
-              Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor eirmod magna dolore erat
-              amet
+            <p className="mb-4">
+              Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor
+              sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem
+              et sit, sed stet no labore lorem sit. Sanctus clita duo justo et
+              tempor eirmod magna dolore erat amet
             </p>
             <div className="row g-0 mb-3">
               <div className="col-sm-6 wow zoomIn" data-wow-delay="0.2s">
-                <h5 className="mb-3"><i className="bi bi-check text-primary me-3"></i>Award Winning</h5>
-                <h5 className="mb-3"><i className="bi bi-check text-primary me-3"></i>Professional Staff</h5>
+                <h5 className="mb-3">
+                  <i className="bi bi-check text-primary me-3"></i>Award Winning
+                </h5>
+                <h5 className="mb-3">
+                  <i className="bi bi-check text-primary me-3"></i>Professional
+                  Staff
+                </h5>
               </div>
               <div className="col-sm-6 wow zoomIn" data-wow-delay="0.4s">
-                <h5 className="mb-3"><i className="bi bi-check text-primary me-3"></i>24/7 Support</h5>
-                <h5 className="mb-3"><i className="bi bi-check text-primary me-3"></i>Fair Prices</h5>
+                <h5 className="mb-3">
+                  <i className="bi bi-check text-primary me-3"></i>24/7 Support
+                </h5>
+                <h5 className="mb-3">
+                  <i className="bi bi-check text-primary me-3"></i>Fair Prices
+                </h5>
               </div>
             </div>
-            <div className="d-flex align-items-center mb-4 wow fadeIn" data-wow-delay="0.6s">
-              <div className="bg-primary d-flex align-items-center justify-content-center rounded"
-                style={{ width: '60px', height: '60px' }}>
+            <div
+              className="d-flex align-items-center mb-4 wow fadeIn"
+              data-wow-delay="0.6s"
+            >
+              <div
+                className="bg-primary d-flex align-items-center justify-content-center rounded"
+                style={{ width: "60px", height: "60px" }}
+              >
                 <i className="bi bi-telephone-fill text-white"></i>
               </div>
               <div className="ps-4">
@@ -39,18 +56,23 @@ const AboutUs = () => {
                 <h4 className="text-primary mb-0">+012 345 6789</h4>
               </div>
             </div>
-            <Link href={'/quote'} className="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">
+            <Link
+              href={"/quote"}
+              className="btn btn-primary py-3 px-5 mt-3 wow zoomIn"
+              data-wow-delay="0.9s"
+            >
               Request A Quote
             </Link>
           </div>
-          <div className="col-lg-5" style={{ minHeight: '500px' }}>
+          <div className="col-lg-5" style={{ minHeight: "500px" }}>
             <div className="position-relative h-100">
               <Image
                 alt="about_us"
                 className="position-absolute w-100 h-100 rounded wow zoomIn"
                 data-wow-delay="0.9s"
                 src={AboutImg}
-                style={{ objectFit: 'cover' }} />
+                style={{ objectFit: "cover" }}
+              />
             </div>
           </div>
         </div>
