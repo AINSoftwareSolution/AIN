ebb7d3f2eaadb0b981753f15805ab2d82bb16106
Naeema bargir <bargirn5@gmail.com>
2024-07-15 14:55:28 +0530

    UI updated

--- src/app/component/index.ts
+++ src/app/component/index.ts
@@ -1,5 +1,7 @@
+import Header from "./header";
 import ServiceCard from "./serviceCard";
+
 import Footer from "./layout/Footer";
 import Navbar from "./layout/Navbar";
 
-export { Footer, Navbar, ServiceCard };
+export { Footer, Navbar, Header, ServiceCard };
